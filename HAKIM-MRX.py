070965f62ee33a216398669b5c8c68b9859e1896
bbb647 <73726317+bbb647@users.noreply.github.com>
2020-11-10 22:14:53 +0200

    Add files via upload
    .

---
+++ HAKIM-MRX.py
@@ -0,0 +1,197 @@
+import os
+os.system("clear")
+print ("============================================")
+print (" _   _       ___   _   _    _       ___  ___  ")
+print ("| | | |     /   | | | / /  | |     /   |/   |")
+print ("|  _  |   / / | | | |\ \   | |   / / |__/ | |")
+print ("| | | |  / /  | | | | \ \  | |  / /       | |")
+print ("|_| |_| /_/   |_| |_|  \_\ |_| /_/        |_|")
+print ("=============================================")
+ 
+#==============================================================
+print ("\033[0;32m            ----------")
+print ("\033[0;32m            Hello dear")
+print ("\033[0;32m            ----------")
+print ("\033[0;32m            welcome")
+print ("\033[0;32m            -------")
+#=============================================================================
+print ("\033[0;32m                       created by HAKIM ")
+print ("\033[0;32m                      ----------------")
+print ("\033[0;32m                       instagram:6.t1s")
+print ("\033[0;32m                       ----------------")
+print ("\033[0;32m                  Youtube channel:HAKIM AL Hacker")
+print ("\033[0;32m                  -------------------------------")
+#============================================================================
+print("\n \033[0;34m[1]Install Hacking Tools ") 
+n = input ("\n Type Number:")
+print(n)
+if n  ==  "1":
+
+    print(" \033[0;34m[1]WhatsappSPAM") 
+    print(" \033[0;34m[2]Holehe")
+    print(" \033[0;34m[3]Striker")
+    print(" \033[0;34m[4]xerxes")
+    print(" \033[0;34m[5]Nikto")
+    print(" \033[0;34m[6]goodlist")
+    print(" \033[0;34m[7]admin-panel-finder")
+    print(" \033[0;34m[8]sqlmap")
+    print(" \033[0;34m[9]guardn")
+    print(" \033[0;34m[10]fb-hacker")
+    print(" \033[0;34m[11]Nmap")
+    print(" \033[0;34m[12]SocialSploit")
+    print(" \033[0;34m[13]metasploit")
+    print(" \033[0;34m[14]RED_HAWK")
+    print(" \033[0;34m[15]tweetshell")
+    print(" \033[0;34m[16]Wolf")
+    print(" \033[0;34m[17]World")
+    print(" \033[0;34m[18]sayhello")
+    print(" \033[0;34m[19]MKBRUTUS")
+    print(" \033[0;34m[20]TBomb")
+    print(" \033[0;34m[21]Virus-droid-creator")
+    print(" \033[0;34m[22]Termux-Style")
+    print(" \033[0;34m[23]Amer")
+    print(" \033[0;34m[24]fakecall")
+    print(" \033[0;34m[25]DarkFly-Tool")
+    print(" \033[0;34m[26]ALIENS.HaCK.Camera")
+    print(" \033[0;34m[27]inshackle")
+    print(" \033[0;34m[28]FbYOchecke")
+    print(" \033[0;34m[29]Cut-X")
+    print(" \033[0;34m[30]TmX-FW")
+    print(" \033[0;34m[31]Ghost")
+    print(" \033[0;34m[32]flb2")
+    print(" \033[0;34m[33]Hash cracker")
+    print(" \033[0;34m[34]Tool-X")
+    print(" \033[0;34m[35]WishFish")                                                                                                       
+    print(" \033[0;34m[36]hack-facebook")
+    print(" \033[0;31m[37]alsharabi-call")
+    print(" \033[0;31m[38]Hunner")
+    print(" \033[0;31m[39]Report")
+    print(" \033[0;31m[40]Card-Number")
+    print(" \033[0;31m[41]HxWhatsApp")
+    print(" \033[0;31m[42]VIRTEXVIRTE")
+    print(" \033[0;31m[43]Mob-Droid")
+    print(" \033[0;31m[44]IP-FY")
+    print(" \033[0;31m[45]brutegmail")
+    print(" \033[0;31m[46]T-Header")
+    print(" \033[0;31m[47]OSIF")
+    print(" \033[0;31m[48]ipcs")
+    print(" \033[0;31m[49]King-Hacking")
+    print(" \033[0;31m[50[wifi-bruteforcer-fsecurify")
+#Codes
+K = input ("\n Type Number:")
+print(K)
+
+
+if K == "1":
+    os.system("git clone https://github.com/sadamshr3be/WhatsappSPAM") 
+if K == "2":
+    os.system("git clone https://github.com/megadose/holehe")
+if K == "3":
+    os.system("git clone https://github.com/s0md3v/Striker")
+if K == "4":
+    os.system("git clone https://github.com/zanyarjamal/xerxes")
+if K == "5":
+    os.system("git clone https://github.com/sullo/nikto")
+if K == "6":
+    os.system("git clone https://github.com/joker0811/goodlist")
+if K == "7":
+    os.system("git clone https://github.com/bdblackhat/admin-panel-finder")
+if K == "8":
+    os.system("git clone https://github.com/sqlmapproject/sqlmap")
+if K == "9":
+    os.system("git clone https://github.com/methaza/guardn")
+if K == "10":
+    os.system("git clone https://github.com/V4N654T/fb-hacker") 
+if K == "11":
+    os.system("pkg install nmap")
+if K == "12":
+    os.system("git clone https://github.com/Cesar-Hack-Gray/SocialSploit")
+if K == "13":
+    os.system("pkg update")
+    os.system("pkg upgrade")
+    os.system("pkg install unstable-repo")
+    os.system("pkg install metasploit")
+if K == "14":
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+if K == "15":
+    os.system("git clone https://github.com/thelinuxchoice/tweetshell")
+if K == "16":
+    os.system("git clone https://github.com/lovehacker404/World")
+if K == "17":
+    os.system("git clone https://github.com/yehia-hacker/WOLF")
+if K == "18":
+    os.system("git clone https://github.com/d093w1z/sayhello")
+if K == "19":
+    os.system("git clone https://github.com/mkbrutusproject/MKBRUTUS")
+if K == "20":
+    os.system("git clone https://github.com/TheSpeedX/TBomb")
+if K == "21":
+    os.system("git clone https://github.com/saydog/Virus-droid-creator")
+if K == "22":
+    os.system("git clone https://github.com/hickin-hack/Termux-Style")
+if K == "23":
+    os.system("git clone https://github.com/Amerlaceset/Amer")
+if K == "24":
+    os.system("git clone https://github.com/siputra12/fakecall")
+if K == "25":
+    os.system("git clone https://github.com/Ranginang67/DarkFly-Tool")
+if K == "26":
+    os.system("git clone https://github.com/hackermohamedPro/ALIENS.HaCK.Camera")
+if K == "27":
+    os.system("git clone https://github.com/thelinuxchoice/inshackle.git")
+if K == "28":
+    os.system("git clone https://github.com/EmadMemo/FbYOchecker")
+if K == "29":
+    os.system("git clone https://github.com/hickin-hack/Cut-X")
+if K == "30":
+    os.system("git clone https://github.com/MoMuilWH/TmX-FW")
+if K == "31":
+    os.system("git clone https://github.com/entynetproject/ghost.git")
+if K == "32":
+    os.system("git clone https://github.com/nasirxo/flb2")
+if K == "33":
+    os.system("git clone https://github.com/CiKu370/hasher")
+if K == "34":
+    os.system("git clone https://github.com/rajkumardusad/Tool-X")
+if K == "35":
+    os.system("pkg install git -y ")
+    os.system("rm -rif ngrok")
+    os.system("rm -rif WishFish")
+    os.system("pkg install php -y")
+    os.system("pkg install openssh -y")
+    os.system("git clone https://github.com/kinghacker0/WishFish")
+    os.system("pkg install wget -y")
+    os.system("wget https://raw.githubusercontent.com/Cesar-Hack-Gray/release-download/master/fix-ar-ngrok && bash fi")
+    os.system("cd WishFish")
+    os.system("cp $PREFIX/bin/ngrok ~/WishFish")
+if K == "36":
+    os.system("git clone https://github.com/sadamshr3be/hack-facebook")
+if K == "37":
+    os.system("git clone https://github.com/sadamshr3be/alsharabi-call")
+if K == "38":
+    os.system("git clone https://github.com/b3-v3r/Hunner")
+if K == "39":
+    os.system("git clone https://github.com/IlayTamvan/Report")
+if K == "40":
+    os.system("git clone https://github.com/INDOnimous/Card-Number")
+if K == "41":
+    os.system("git clone https://github.com/Bl4ckDr460n/HxWhatsApp")
+if K == "42":
+    os.system("git clone https://github.com/muhammadfathul/VIRTEXVIRTE")
+if K == "43":
+    os.system("git clone https://github.com/kinghacker0/Mob-Droid")
+if K == "44":
+    os.system("git clone https://github.com/T4P4N/IP-FY")
+if K == "45":
+    os.system("git clone https://github.com/NeloF4/brutegmail")
+if K == "46":
+    os.system("git clone https://github.com/remo7777/THeader")
+if K == "47":
+    os.system("git clone https://github.com/ciku370/OSIF")
+if K == "48":
+    os.system("git clone https://github.com/kancotdiq/ipcs")
+if K == "49":
+    os.system("git clone https://github.com/king-hacking/King-Hacking")
+if K == "50":
+    os.system("git clone https://github.com/faizann24/wifi-bruteforcer-fsecurify")
+
